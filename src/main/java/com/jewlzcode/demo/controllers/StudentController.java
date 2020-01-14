29074de093f9df0784dd7e5c9b80efa137539cb0
Julian Murillo <jomsk822@gmail.com>
2020-01-14 14:33:06 -0500

    Added functionality for persisting added student to db
    Added functionality for persisting added student to db

--- src/main/java/com/jewlzcode/demo/controllers/StudentController.java
+++ src/main/java/com/jewlzcode/demo/controllers/StudentController.java
@@ -22,7 +22,7 @@ public class StudentController {
 
     @PostMapping
     public void addNewStudent(@RequestBody Student student) {
-        System.out.println(student);
+        studentService.addNewStudent(student);
     }
 
 }
--- src/main/java/com/jewlzcode/demo/services/StudentService.java
+++ src/main/java/com/jewlzcode/demo/services/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -15,5 +16,12 @@ public class StudentService {
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+
+        // TODO: Verify that email is not taken
+
+        studentRepository.save(student);
     }
 }
